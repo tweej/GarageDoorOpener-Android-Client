aa05d69b0207f842758cf20b69553d25d24be478
TJ Mercier <thomas.mercier.jr@gmail.com>
2016-03-20 12:06:09 -0400

    Work around Android SDK reverse DNS lookup bug

--- GarageDoorOpenerClient/src/main/java/teej/garagedooropenerclient/KeypadActivity.java
+++ GarageDoorOpenerClient/src/main/java/teej/garagedooropenerclient/KeypadActivity.java
@@ -23,8 +23,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Field;
 import java.net.ConnectException;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.security.KeyManagementException;
@@ -40,6 +42,8 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.SocketFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSocket;
@@ -323,9 +327,58 @@ public class KeypadActivity extends Activity implements
                 SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
                 sslContext.init(null, new TrustManager[]{tm}, null);
 
+                Log.d("GDO", "Creating socket factory...");
+                // http://stackoverflow.com/questions/4737019/tls-connection-using-sslsocket-is-slow-in-android-os
+                final SSLSocketFactory delegate = sslContext.getSocketFactory();
+                SocketFactory factory = new SSLSocketFactory() {
+                    @Override
+                    public Socket createSocket(String host, int port)
+                            throws IOException, UnknownHostException {
+                        InetAddress addr = InetAddress.getByName(host);
+                        injectHostname(addr, host);
+                        return delegate.createSocket(addr, port);
+                    }
+                    @Override
+                    public Socket createSocket(InetAddress host, int port)
+                            throws IOException {
+                        return delegate.createSocket(host, port);
+                    }
+                    @Override
+                    public Socket createSocket(String host, int port, InetAddress localHost, int localPort)
+                            throws IOException, UnknownHostException {
+                        return delegate.createSocket(host, port, localHost, localPort);
+                    }
+                    @Override
+                    public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort)
+                            throws IOException {
+                        return delegate.createSocket(address, port, localAddress, localPort);
+                    }
+                    private void injectHostname(InetAddress address, String host) {
+                        try {
+                            Field field = InetAddress.class.getDeclaredField("hostName");
+                            field.setAccessible(true);
+                            field.set(address, host);
+                        } catch (Exception ignored) {
+                        }
+                    }
+                    @Override
+                    public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+                        injectHostname(s.getInetAddress(), host);
+                        return delegate.createSocket(s, host, port, autoClose);
+                    }
+                    @Override
+                    public String[] getDefaultCipherSuites() {
+                        return delegate.getDefaultCipherSuites();
+                    }
+                    @Override
+                    public String[] getSupportedCipherSuites() {
+                        return delegate.getSupportedCipherSuites();
+                    }
+                };
+
                 Log.d("GDO", "Creating socket...");
-                c = (SSLSocket) sslContext.getSocketFactory().createSocket(
-                        InetAddress.getByName(sharedPref.getString("IP", "")).getHostAddress(),
+                c = (SSLSocket) factory.createSocket(
+                        sharedPref.getString("IP", ""),
                         Integer.parseInt(sharedPref.getString("PORT", "")));
 
                 c.setEnabledProtocols(new String[]{"TLSv1.2"});
